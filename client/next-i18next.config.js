d8d7ce4436b7d1c2170e5d9b667a4efc3365bf91
jackiexiao <707610215@qq.com>
2023-09-05 17:37:45 +0800

    [feat] set default locale from env

--- client/next-i18next.config.js
+++ client/next-i18next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     locales: ['en', 'zh'],
-    defaultLocale: 'en',
+    defaultLocale: process.env.DEFAULT_LOCALE || 'en',
   },
   localePath: path.resolve('./public/locales'),
 };
